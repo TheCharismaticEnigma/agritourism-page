77ac97caf0023425565b5b9c62017a7328ce8a62
TheCharismaticEnigma <avishkar2002@mail.com>
2023-12-09 19:46:35 +0530

    Refactor: Add Logo to the App

--- app/(Header)/NavBar.tsx
+++ app/(Header)/NavBar.tsx
@@ -1,4 +1,6 @@
-import { Button, Flex, Text } from '@chakra-ui/react';
+import logo from '@/public/logo.webp';
+import { Button, Flex } from '@chakra-ui/react';
+import Image from 'next/image';
 import Link from 'next/link';
 interface NavLink {
   label: string;
@@ -20,31 +22,24 @@ const NavBar = () => {
       top={0}
       left={0}
       width={'100%'}
-      className="rounded-b-3xl  shadow-gray-400 shadow-inner px-4 md:px-8 py-3 bg-gradient-to-br from-[#00000022] to-[#00000033] backdrop-saturate-200 backdrop-blur-[20px]"
+      className="rounded-b-3xl  shadow-gray-400 shadow-inner px-4 md:px-8 py-3 bg-gradient-to-tl from-[#00000022] to-[#00000043] backdrop-saturate-200 backdrop-blur-[20px]"
     >
-      <Text
-        color={'green.900'}
-        fontSize={{
-          base: '1rem',
-          sm: '1rem',
-          md: '1.5rem',
-        }}
-        fontWeight={'bold'}
-        fontFamily={'cursive'}
-      >
-        AGSPERT
-      </Text>
+      <Link href={'#'} className="bg-inherit">
+        <Image className="bg-red" width={180} src={logo} alt="Agspeak Logo" />
+      </Link>
 
       <Flex
         width={{
-          md: '50%',
+          md: '55%',
         }}
         display={{
           base: 'none',
           sm: 'flex',
         }}
         justifyContent={'space-around'}
-        gap={5}
+        gap={{
+          sm: 4,
+        }}
       >
         {links.map(({ label, href }) => (
           <NavLink key={label} label={label} href={href} />
